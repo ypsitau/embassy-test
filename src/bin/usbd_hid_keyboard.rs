70e8d63c770a1a02c3e17d464e7727faaa67f732
ypsitau <ypsitau@nifty.com>
2026-08-13 15:53:35 +0900

    implementing hid_keyboard

--- src/bin/usbd_hid_keyboard.rs
+++ src/bin/usbd_hid_keyboard.rs
@@ -20,13 +20,11 @@ bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => usb::InterruptHandler<peripherals::USB>;
 });
 
-static HID_PROTOCOL_MODE: AtomicU8 = AtomicU8::new(usb_class::hid::HidProtocolMode::Boot as u8);
-
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let driver = usb::Driver::new(p.USB, Irqs);
-    let mut builder = {
+    let usb_driver = usb::Driver::new(p.USB, Irqs);
+    let mut usb_builder = {
         let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
         config.manufacturer = Some("Embassy");
         config.product = Some("HID keyboard example");
@@ -41,19 +39,19 @@ async fn main(_spawner: Spawner) {
         static BOS_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
         static MSOS_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
         static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
-        static DEVICE_HANDLER: StaticCell<MyDeviceHandler> = StaticCell::new();
-        let mut builder = embassy_usb::Builder::new(
-            driver,
+        let mut usb_builder = embassy_usb::Builder::new(
+            usb_driver,
             config,
             CONFIG_DESCRIPTOR.init([0; 256]),
             BOS_DESCRIPTOR.init([0; 256]),
             MSOS_DESCRIPTOR.init([0; 256]),
             CONTROL_BUF.init([0; 64]),
         );
-        builder.handler(DEVICE_HANDLER.init(MyDeviceHandler::new()));
-        builder
+        static DEVICE_HANDLER: StaticCell<DeviceHandler> = StaticCell::new();
+        usb_builder.handler(DEVICE_HANDLER.init(DeviceHandler::new()));
+        usb_builder
     };
-    let hid_reader_writer = {
+    let (hid_reader, mut hid_writer) = {
         static STATE: StaticCell<usb_class::hid::State> = StaticCell::new();
         let config = embassy_usb::class::hid::Config {
             report_descriptor: usbd_hid::descriptor::KeyboardReport::desc(),
@@ -64,95 +62,110 @@ async fn main(_spawner: Spawner) {
             hid_boot_protocol: usb_class::hid::HidBootProtocol::Keyboard,
         };
         usb_class::hid::HidReaderWriter::<_, 1, 8>::new(
-            &mut builder,
+            &mut usb_builder,
             STATE.init(usb_class::hid::State::new()),
             config,
-        )
+        ).split()
     };
-    let mut usb = builder.build();
-    let usb_fut = usb.run();
-    let mut gpio_signal = gpio::Input::new(p.PIN_16, gpio::Pull::Up);
-    let (hid_reader, mut hid_writer) = hid_reader_writer.split();
-    let hid_writer_fut = async {
+    let mut usb_device = usb_builder.build();
+    let fut_usb = usb_device.run();
+    let fut_hid_writer = async {
+        let mut gpio_button_left = gpio::Input::new(p.PIN_18, gpio::Pull::Up);
+        let mut gpio_button_up = gpio::Input::new(p.PIN_19, gpio::Pull::Up);
+        let mut gpio_button_down = gpio::Input::new(p.PIN_20, gpio::Pull::Up);
+        let mut gpio_button_right = gpio::Input::new(p.PIN_21, gpio::Pull::Up);
         loop {
-            gpio_signal.wait_for_any_edge().await;
-            Timer::after_millis(100).await; // skip the bounding period
-            let key_code = match gpio_signal.get_level() {
-                gpio::Level::High => { info!("HIGH DETECTED"); 0 },
-                gpio::Level::Low => { info!("LOW DETECTED"); 4 },
+            embassy_futures::select::select_array([
+                gpio_button_left.wait_for_any_edge(),
+                gpio_button_up.wait_for_any_edge(),
+                gpio_button_down.wait_for_any_edge(),
+                gpio_button_right.wait_for_any_edge(),
+            ]).await;
+            Timer::after_millis(30).await; // skip the bounding period
+            let mut n_keycodes = 0;
+            let mut keycodes: [u8; 6] = [0; 6];
+            if gpio_button_left.is_low() { keycodes[n_keycodes] = 0x50; n_keycodes += 1; }
+            if gpio_button_up.is_low() { keycodes[n_keycodes] = 0x52; n_keycodes += 1; }
+            if gpio_button_down.is_low() { keycodes[n_keycodes] = 0x51; n_keycodes += 1; }
+            if gpio_button_right.is_low() { keycodes[n_keycodes] = 0x4f; /* n_keycodes += 1; */ }
+            let keyboard_report = usbd_hid::descriptor::KeyboardReport {
+                modifier: 0,
+                reserved: 0,
+                leds: 0,
+                keycodes,
             };
-            if HID_PROTOCOL_MODE.load(Ordering::Relaxed) == usb_class::hid::HidProtocolMode::Boot as u8 {
-                if let Err(e) = hid_writer.write(&[0, 0, key_code, 0, 0, 0, 0, 0]).await {
-                    warn!("Failed to send boot report: {:?}", e);
-                }
-            } else {
-                let report = usbd_hid::descriptor::KeyboardReport {
-                    keycodes: [key_code, 0, 0, 0, 0, 0],
-                    leds: 0,
-                    modifier: 0,
-                    reserved: 0,
-                };
-                if let Err(e) = hid_writer.write_serialize(&report).await {
-                    warn!("Failed to send report: {:?}", e);
-                }
+            println!("Sending report: {:?}", keycodes);
+            if let Err(e) = hid_writer.write_serialize(&keyboard_report).await {
+                warn!("Failed to send report: {:?}", e);
             }
         }
     };
-    let hid_reader_fut = async {
-        let mut request_handler = MyRequestHandler;
+    let fut_hid_reader = async {
+        let mut request_handler = RequestHandler::new();
         hid_reader.run(false, &mut request_handler).await;
     };
-    join(usb_fut, join(hid_writer_fut, hid_reader_fut)).await;
+    join(fut_usb, join(fut_hid_writer, fut_hid_reader)).await;
 }
 
-struct MyRequestHandler;
+//-----------------------------------------------------------------------------
+// RequestHandler
+//-----------------------------------------------------------------------------
+struct RequestHandler {
+    hid_protocol_mode: AtomicU8,
+}
 
-impl usb_class::hid::RequestHandler for MyRequestHandler {
+impl RequestHandler {
+    fn new() -> Self {
+        RequestHandler {
+            hid_protocol_mode: AtomicU8::new(usb_class::hid::HidProtocolMode::Boot as u8),
+        }
+    }
+}
+
+impl usb_class::hid::RequestHandler for RequestHandler {
     fn get_report(&mut self, id: usb_class::hid::ReportId, _buf: &mut [u8]) -> Option<usize> {
         info!("Get report for {:?}", id);
         None
     }
-
     fn set_report(&mut self, id: usb_class::hid::ReportId, data: &[u8]) -> embassy_usb::control::OutResponse {
         info!("Set report for {:?}: {=[u8]}", id, data);
         embassy_usb::control::OutResponse::Accepted
     }
-
     fn get_protocol(&self) -> usb_class::hid::HidProtocolMode {
-        let protocol = usb_class::hid::HidProtocolMode::from(HID_PROTOCOL_MODE.load(Ordering::Relaxed));
-        info!("The current HID protocol mode is: {}", protocol);
-        protocol
+        let mode = usb_class::hid::HidProtocolMode::from(self.hid_protocol_mode.load(Ordering::Relaxed));
+        info!("The current HID protocol mode is: {}", mode);
+        mode
     }
-
     fn set_protocol(&mut self, protocol: usb_class::hid::HidProtocolMode) -> embassy_usb::control::OutResponse {
         info!("Switching to HID protocol mode: {}", protocol);
-        HID_PROTOCOL_MODE.store(protocol as u8, Ordering::Relaxed);
+        self.hid_protocol_mode.store(protocol as u8, Ordering::Relaxed);
         embassy_usb::control::OutResponse::Accepted
     }
-
     fn set_idle_ms(&mut self, id: Option<usb_class::hid::ReportId>, dur: u32) {
         info!("Set idle rate for {:?} to {:?}", id, dur);
     }
-
     fn get_idle_ms(&mut self, id: Option<usb_class::hid::ReportId>) -> Option<u32> {
         info!("Get idle rate for {:?}", id);
         None
     }
 }
 
-struct MyDeviceHandler {
+//-----------------------------------------------------------------------------
+// DeviceHandler
+//-----------------------------------------------------------------------------
+struct DeviceHandler {
     configured: AtomicBool,
 }
 
-impl MyDeviceHandler {
+impl DeviceHandler {
     fn new() -> Self {
-        MyDeviceHandler {
+        DeviceHandler {
             configured: AtomicBool::new(false),
         }
     }
 }
 
-impl embassy_usb::Handler for MyDeviceHandler {
+impl embassy_usb::Handler for DeviceHandler {
     fn enabled(&mut self, enabled: bool) {
         self.configured.store(false, Ordering::Relaxed);
         if enabled {
@@ -161,17 +174,14 @@ impl embassy_usb::Handler for MyDeviceHandler {
             info!("Device disabled");
         }
     }
-
     fn reset(&mut self) {
         self.configured.store(false, Ordering::Relaxed);
         info!("Bus reset, the Vbus current limit is 100mA");
     }
-
     fn addressed(&mut self, addr: u8) {
         self.configured.store(false, Ordering::Relaxed);
         info!("USB address set to: {}", addr);
     }
-
     fn configured(&mut self, configured: bool) {
         self.configured.store(configured, Ordering::Relaxed);
         if configured {
