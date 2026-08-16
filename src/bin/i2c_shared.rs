d7b8f942370ec84e52a7650244bbee2d8ef0c1d8
ypsitau <ypsitau@nifty.com>
2026-08-16 17:18:16 +0900

    updated

--- src/bin/i2c_shared.rs
+++ src/bin/i2c_shared.rs
@@ -1,5 +1,3 @@
-//! This example shows how to share (async) I2C and SPI buses between multiple devices.
-
 #![no_std]
 #![no_main]
 
@@ -7,13 +5,11 @@ use defmt::*;
 use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
 use embassy_executor::Spawner;
 use embassy_rp as rp;
-//use embassy_rp::i2c::{self, I2c, InterruptHandler};
-//use embassy_rp::peripherals::{I2C1};
-//use embassy_rp::{bind_interrupts};
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::mutex::Mutex;
 use embassy_time::Timer;
 use static_cell::StaticCell;
+use ssd1306::prelude::*;
 use {defmt_rtt as _, panic_probe as _};
 
 type I2c1Bus = Mutex<NoopRawMutex, rp::i2c::I2c<'static, rp::peripherals::I2C1, rp::i2c::Async>>;
@@ -24,23 +20,21 @@ rp::bind_interrupts!(struct Irqs {
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    let p = embassy_rp::init(Default::default());
-    info!("Here we go!");
-
-    // Shared I2C bus
-    let i2c = rp::i2c::I2c::new_async(p.I2C1, p.PIN_15, p.PIN_14, Irqs, rp::i2c::Config::default());
-    static I2C_BUS: StaticCell<I2c1Bus> = StaticCell::new();
-    let i2c_bus = I2C_BUS.init(Mutex::new(i2c));
-
-    spawner.spawn(i2c_task_a(i2c_bus).unwrap());
-    spawner.spawn(i2c_task_b(i2c_bus).unwrap());
-
+    let p = rp::init(Default::default());
+    let i2c_bus = {
+        let config = rp::i2c::Config::default();
+        let i2c = rp::i2c::I2c::new_async(p.I2C1, p.PIN_15, p.PIN_14, Irqs, config);
+        static I2C_BUS: StaticCell<I2c1Bus> = StaticCell::new();
+        I2C_BUS.init(Mutex::new(i2c))
+    };
+    spawner.spawn(task_a(i2c_bus).unwrap());
+    spawner.spawn(task_b(i2c_bus).unwrap());
+    spawner.spawn(task_c(i2c_bus).unwrap());
 }
 
 #[embassy_executor::task]
-async fn i2c_task_a(i2c_bus: &'static I2c1Bus) {
-    let i2c_dev = I2cDevice::new(i2c_bus);
-    let _sensor = DummyI2cDeviceDriver::new(i2c_dev, 0xC0);
+async fn task_a(i2c_bus: &'static I2c1Bus) {
+    let _sensor = DummyI2cDeviceDriver::new(I2cDevice::new(i2c_bus), 0xC0);
     loop {
         info!("i2c task A");
         Timer::after_secs(1).await;
@@ -48,22 +42,31 @@ async fn i2c_task_a(i2c_bus: &'static I2c1Bus) {
 }
 
 #[embassy_executor::task]
-async fn i2c_task_b(i2c_bus: &'static I2c1Bus) {
-    let i2c_dev = I2cDevice::new(i2c_bus);
-    let _sensor = DummyI2cDeviceDriver::new(i2c_dev, 0xDE);
+async fn task_b(i2c_bus: &'static I2c1Bus) {
+    let _sensor = DummyI2cDeviceDriver::new(I2cDevice::new(i2c_bus), 0xDE);
     loop {
         info!("i2c task B");
         Timer::after_secs(1).await;
     }
 }
 
-// Dummy I2C device driver, using `embedded-hal-async`
-struct DummyI2cDeviceDriver<I2C: embedded_hal_async::i2c::I2c> {
-    _i2c: I2C,
+#[embassy_executor::task]
+async fn task_c(i2c_bus: &'static I2c1Bus) {
+    let mut display = {
+        let interface = ssd1306::I2CDisplayInterface::new(I2cDevice::new(i2c_bus));
+        ssd1306::Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+            .into_buffered_graphics_mode()
+    };
+    display.init().await.unwrap();
+    
 }
 
-impl<I2C: embedded_hal_async::i2c::I2c> DummyI2cDeviceDriver<I2C> {
-    fn new(i2c_dev: I2C, _address: u8) -> Self {
-        Self { _i2c: i2c_dev }
+struct DummyI2cDeviceDriver<I2cDev: embedded_hal_async::i2c::I2c> {
+    _i2c_dev: I2cDev,
+}
+    
+impl<I2cDev: embedded_hal_async::i2c::I2c> DummyI2cDeviceDriver<I2cDev> {
+    fn new(i2c_dev: I2cDev, _address: u8) -> Self {
+        Self { _i2c_dev: i2c_dev }
     }
 }
