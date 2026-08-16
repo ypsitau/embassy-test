55c6cb2c2e423828d51f0bbca3102415cd9f71ec
ypsitau <ypsitau@nifty.com>
2026-08-16 13:26:00 +0900

    renamed

--- src/bin/pwm_assign.rs
+++ src/bin/pwm_assign.rs
@@ -20,9 +20,15 @@ async fn main(_spawner: Spawner) {
     //assign_pwm_b_second_half(p, config.clone());
     //assign_pwm_ab_first_half(p, config.clone());
     //assign_pwm_ab_second_half(p, config.clone());
+} 
+
+// a function that can take rp::pwm::Pwm and rp:pwm::PwmOutput
+fn test_func(mut pwm: impl rp::pwm::SetDutyCycle) {
+    pwm.set_duty_cycle_fraction(50, 100).unwrap();
 }
 
 fn assign_pwm_a_first_half(p: rp::Peripherals, config: rp::pwm::Config) {
+    // create rp::pwm::Pwm instances
     let mut pwm_0 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE0, p.PIN_0, config.clone());
     let mut pwm_2 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE1, p.PIN_2, config.clone());
     let mut pwm_4 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE2, p.PIN_4, config.clone());
@@ -31,17 +37,18 @@ fn assign_pwm_a_first_half(p: rp::Peripherals, config: rp::pwm::Config) {
     let mut pwm_10 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE5, p.PIN_10, config.clone());
     let mut pwm_12 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE6, p.PIN_12, config.clone());
     let mut pwm_14 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE7, p.PIN_14, config.clone());
-    pwm_0.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_2.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_4.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_6.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_8.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_10.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_12.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_14.set_duty_cycle_fraction(50, 100).unwrap();
+    test_func(pwm_0);
+    test_func(pwm_2);
+    test_func(pwm_4);
+    test_func(pwm_6);
+    test_func(pwm_8);
+    test_func(pwm_10);
+    test_func(pwm_12);
+    test_func(pwm_14);
 }
 
 fn assign_pwm_a_second_half(p: rp::Peripherals, config: rp::pwm::Config) {
+    // create rp::pwm::Pwm instances
     let mut pwm_16 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE0, p.PIN_16, config.clone());
     let mut pwm_18 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE1, p.PIN_18, config.clone());
     let mut pwm_20 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE2, p.PIN_20, config.clone());
@@ -49,16 +56,17 @@ fn assign_pwm_a_second_half(p: rp::Peripherals, config: rp::pwm::Config) {
     let mut pwm_24 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE4, p.PIN_24, config.clone());
     let mut pwm_26 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE5, p.PIN_26, config.clone());
     let mut pwm_28 = rp::pwm::Pwm::new_output_a(p.PWM_SLICE6, p.PIN_28, config.clone());
-    pwm_16.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_18.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_20.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_22.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_24.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_26.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_28.set_duty_cycle_fraction(50, 100).unwrap();
+    test_func(pwm_16);
+    test_func(pwm_18);
+    test_func(pwm_20);
+    test_func(pwm_22);
+    test_func(pwm_24);
+    test_func(pwm_26);
+    test_func(pwm_28);
 }
 
 fn assign_pwm_b_first_half(p: rp::Peripherals, config: rp::pwm::Config) {
+    // create rp::pwm::Pwm instances
     let mut pwm_1 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE0, p.PIN_1, config.clone());
     let mut pwm_3 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE1, p.PIN_3, config.clone());
     let mut pwm_5 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE2, p.PIN_5, config.clone());
@@ -67,17 +75,18 @@ fn assign_pwm_b_first_half(p: rp::Peripherals, config: rp::pwm::Config) {
     let mut pwm_11 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE5, p.PIN_11, config.clone());
     let mut pwm_13 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE6, p.PIN_13, config.clone());
     let mut pwm_15 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE7, p.PIN_15, config.clone());
-    pwm_1.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_3.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_5.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_7.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_9.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_11.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_13.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_15.set_duty_cycle_fraction(50, 100).unwrap();
+    test_func(pwm_1);
+    test_func(pwm_3);
+    test_func(pwm_5);
+    test_func(pwm_7);
+    test_func(pwm_9);
+    test_func(pwm_11);
+    test_func(pwm_13);
+    test_func(pwm_15);
 }
 
 fn assign_pwm_b_second_half(p: rp::Peripherals, config: rp::pwm::Config) {
+    // create rp::pwm::Pwm instances
     let mut pwm_17 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE0, p.PIN_17, config.clone());
     let mut pwm_19 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE1, p.PIN_19, config.clone());
     let mut pwm_21 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE2, p.PIN_21, config.clone());
@@ -85,16 +94,17 @@ fn assign_pwm_b_second_half(p: rp::Peripherals, config: rp::pwm::Config) {
     let mut pwm_25 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE4, p.PIN_25, config.clone());
     let mut pwm_27 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE5, p.PIN_27, config.clone());
     let mut pwm_29 = rp::pwm::Pwm::new_output_b(p.PWM_SLICE6, p.PIN_29, config.clone());
-    pwm_17.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_19.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_21.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_23.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_25.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_27.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_29.set_duty_cycle_fraction(50, 100).unwrap();
+    test_func(pwm_17);
+    test_func(pwm_19);
+    test_func(pwm_21);
+    test_func(pwm_23);
+    test_func(pwm_25);
+    test_func(pwm_27);
+    test_func(pwm_29);
 }
 
 fn assign_pwm_ab_first_half(p: rp::Peripherals, config: rp::pwm::Config) {
+    // create rp::pwm::PwmOutput instances
     let (Some(mut pwm_0), Some(mut pwm_1)) = rp::pwm::Pwm::new_output_ab(
         p.PWM_SLICE0, p.PIN_0, p.PIN_1, config.clone()).split() else { panic!(); };
     let (Some(mut pwm_2), Some(mut pwm_3)) = rp::pwm::Pwm::new_output_ab(
@@ -111,25 +121,26 @@ fn assign_pwm_ab_first_half(p: rp::Peripherals, config: rp::pwm::Config) {
         p.PWM_SLICE6, p.PIN_12, p.PIN_13, config.clone()).split() else { panic!(); };
     let (Some(mut pwm_14), Some(mut pwm_15)) = rp::pwm::Pwm::new_output_ab(
         p.PWM_SLICE7, p.PIN_14, p.PIN_15, config.clone()).split() else { panic!(); };
-    pwm_0.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_1.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_2.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_3.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_4.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_5.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_6.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_7.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_8.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_9.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_10.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_11.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_12.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_13.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_14.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_15.set_duty_cycle_fraction(50, 100).unwrap();
+    test_func(pwm_0);
+    test_func(pwm_1);
+    test_func(pwm_2);
+    test_func(pwm_3);
+    test_func(pwm_4);
+    test_func(pwm_5);
+    test_func(pwm_6);
+    test_func(pwm_7);
+    test_func(pwm_8);
+    test_func(pwm_9);
+    test_func(pwm_10);
+    test_func(pwm_11);
+    test_func(pwm_12);
+    test_func(pwm_13);
+    test_func(pwm_14);
+    test_func(pwm_15);
 }
 
 fn assign_pwm_ab_second_half(p: rp::Peripherals, config: rp::pwm::Config) {
+    // create rp::pwm::PwmOutput instances
     let (Some(mut pwm_16), Some(mut pwm_17)) = rp::pwm::Pwm::new_output_ab(
         p.PWM_SLICE0, p.PIN_16, p.PIN_17, config.clone()).split() else { panic!(); };
     let (Some(mut pwm_18), Some(mut pwm_19)) = rp::pwm::Pwm::new_output_ab(
@@ -144,20 +155,20 @@ fn assign_pwm_ab_second_half(p: rp::Peripherals, config: rp::pwm::Config) {
         p.PWM_SLICE5, p.PIN_26, p.PIN_27, config.clone()).split() else { panic!(); };
     let (Some(mut pwm_28), Some(mut pwm_29)) = rp::pwm::Pwm::new_output_ab(
         p.PWM_SLICE6, p.PIN_28, p.PIN_29, config.clone()).split() else { panic!(); };
-    pwm_16.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_17.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_18.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_19.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_20.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_21.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_22.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_23.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_24.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_25.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_26.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_27.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_28.set_duty_cycle_fraction(50, 100).unwrap();
-    pwm_29.set_duty_cycle_fraction(50, 100).unwrap();
+    test_func(pwm_16);
+    test_func(pwm_17);
+    test_func(pwm_18);
+    test_func(pwm_19);
+    test_func(pwm_20);
+    test_func(pwm_21);
+    test_func(pwm_22);
+    test_func(pwm_23);
+    test_func(pwm_24);
+    test_func(pwm_25);
+    test_func(pwm_26);
+    test_func(pwm_27);
+    test_func(pwm_28);
+    test_func(pwm_29);
 }
 
 fn pwm_calc_divider_and_top(clk_sys_freq: u32, freq: u32) -> (f32, u16) {
