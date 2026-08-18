18783e0576a993753ba63e64a5cccd49e5463a0e
ypsitau <ypsitau@nifty.com>
2026-08-18 16:53:01 +0900

    imlementing ili9341

--- src/bin/spi_ili9341.rs
+++ src/bin/spi_ili9341.rs
@@ -119,10 +119,17 @@ async fn main(_spawner: embassy_executor::Spawner) {
             time::Timer::after_millis(30).await;
         }
     };
+    //let fut_touch = async {
+    //    loop {
+    //        if let Some((x, y)) = touch.read_pos() {
+    //            defmt::info!("x = {:04}, y = {:04}", x, y);
+    //        }
+    //    }
+    //};
     let fut_touch = async {
         let mut idx_write = 0;
         let mut idx_read = 0;
-        let mut pos_buf: [Pos; 16] = [Pos { x: 0, y: 0 }; 16];
+        let mut pos_buf: [Pos; 8] = [Pos { x: 0, y: 0 }; 8];
         loop {
             if let Some((x, y)) = touch.read_pos() {
                 pos_buf[idx_write] = Pos { x, y };
@@ -167,17 +174,19 @@ mod xpt2046 {
         y_range: i32,
     }
     impl Calibration {
-        fn calc_pos(&self, xraw: i32, yraw: i32) -> Option<(i32, i32)> {
-            let x = ((xraw - self.xraw_min) * self.x_range / (self.xraw_max - self.xraw_min)).clamp(0, self.x_range);
-            let y = ((yraw - self.yraw_min) * self.y_range / (self.yraw_max - self.yraw_min)).clamp(0, self.y_range);
-            if x == 0 && y == 0 { None } else { Some((x, y)) }
+        fn calc_pos(&self, xraw: i32, yraw: i32) -> (i32, i32) {
+            let x = ((xraw - self.xraw_min) * self.x_range /
+                (self.xraw_max - self.xraw_min)).clamp(0, self.x_range);
+            let y = ((yraw - self.yraw_min) * self.y_range /
+                (self.yraw_max - self.yraw_min)).clamp(0, self.y_range);
+            (x, y)
         }
     }
     const CALIBRATION: Calibration = Calibration {
-        xraw_min: 340,
-        xraw_max: 3880,
-        yraw_min: 262,
-        yraw_max: 3850,
+        xraw_min: 0x00c8,
+        xraw_max: 0x0760,
+        yraw_min: 0x00d0,
+        yraw_max: 0x06d0,
         x_range: 320,
         y_range: 240,
     };
@@ -191,15 +200,27 @@ mod xpt2046 {
         pub fn read_pos(&mut self) -> Option<(i32, i32)> {
             let mut xbytes = [0u8; 2];
             let mut ybytes = [0u8; 2];
+            let mut zbytes = [0u8; 1];
             self.spi_device.transaction(&mut [
-                hal::spi::Operation::Write(&[0x90]),
+                hal::spi::Operation::Write(&[Self::compose_cmd(0b001, 0b0, 0b1, 0b01)]), // x
                 hal::spi::Operation::Read(&mut xbytes),
-                hal::spi::Operation::Write(&[0xd0]),
+                hal::spi::Operation::Write(&[Self::compose_cmd(0b101, 0b0, 0b1, 0b01)]), // y
                 hal::spi::Operation::Read(&mut ybytes),
+                hal::spi::Operation::Write(&[Self::compose_cmd(0b011, 0b1, 0b1, 0b01)]), // z
+                hal::spi::Operation::Read(&mut zbytes),
             ]).unwrap();
-            let xraw = (u16::from_be_bytes(xbytes) >> 3) as i32;
-            let yraw = (u16::from_be_bytes(ybytes) >> 3) as i32;
-            CALIBRATION.calc_pos(xraw, yraw)
+            //info!("xbytes: {:02x}, ybytes: {:02x}, zbytes: {:02x}", xbytes, ybytes, zbytes);
+            let xraw = (((xbytes[0] as i32) << 4) | (xbytes[1] as i32 >> 4)) as i32;
+            let yraw = (((ybytes[0] as i32) << 4) | (ybytes[1] as i32 >> 4)) as i32;
+            if zbytes[0] < 3 {
+                None
+            } else {
+                //info!("xraw: {:04x}, yraw: {:04x}, zbytes: {:02x}", xraw, yraw, zbytes);
+                Some(CALIBRATION.calc_pos(xraw, yraw))
+            }
+        }
+        const fn compose_cmd(adc: u8, mode: u8, reference: u8, power_down_mode: u8) -> u8 {
+            (0b1 << 7) | (adc << 4) | (mode << 3) | (reference << 2) | (power_down_mode << 0)
         }
     }
 }
