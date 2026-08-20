e7b9f5425b3f6856bded65d77dcd3dc1362aab10
ypsitau <ypsitau@nifty.com>
2026-08-20 16:48:27 +0900

    implementing

--- src/bin/spi_ili9341.rs
+++ src/bin/spi_ili9341.rs
@@ -13,6 +13,8 @@ use mipidsi::models::ILI9341Rgb565 as DisplayModel;
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
+use embassy_test::xpt2046;
+
 //rp::bind_interrupts!(struct Irqs {
 //    DMA_IRQ_0 =>
 //        rp::dma::InterruptHandler<rp::peripherals::DMA_CH0>,
@@ -26,6 +28,35 @@ use {defmt_rtt as _, panic_probe as _};
 type MutexSPI1 = embassy_sync::blocking_mutex::Mutex<
     embassy_sync::blocking_mutex::raw::NoopRawMutex,
     RefCell<rp::spi::Spi<'static, rp::peripherals::SPI1, rp::spi::Blocking>>>;
+
+type MutexPos = embassy_sync::blocking_mutex::Mutex<
+    embassy_sync::blocking_mutex::raw::NoopRawMutex,
+    RefCell<Option<xpt2046::Pos>>>;
+
+struct SharedPos {
+    mutex_pos: MutexPos,
+}
+
+impl SharedPos {
+    fn new() -> Self {
+        Self {
+            mutex_pos: MutexPos::new(RefCell::new(None)),
+        }
+    }
+}
+
+impl xpt2046::SharedPos for SharedPos {
+    fn get_pos(&self) -> Option<xpt2046::Pos> {
+        self.mutex_pos.lock(|p| *p.borrow())
+    }
+    fn set_pos(&self, pos: Option<xpt2046::Pos>) {
+        self.mutex_pos.lock(|p| {
+            *p.borrow_mut() = pos;
+        });
+    }
+}
+
+use xpt2046::SharedPos as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: embassy_executor::Spawner) {
@@ -41,7 +72,7 @@ async fn main(_spawner: embassy_executor::Spawner) {
         let spi = rp::spi::Spi::new_blocking(p.SPI1, clk, mosi, miso, config);
         MutexSPI1::new(RefCell::new(spi))
     };
-    let mutex_pos = xpt2046::MutexPos::new(RefCell::new(None));
+    let shared_pos = SharedPos::new();
     let fut_touch = {
         let spi_device = {
             let gpio_cs = rp::gpio::Output::new(p.PIN_14, rp::gpio::Level::High);
@@ -52,7 +83,7 @@ async fn main(_spawner: embassy_executor::Spawner) {
             config.polarity = rp::spi::Polarity::IdleHigh;
             hal::shared_bus::blocking::spi::SpiDeviceWithConfig::new(&mutex_spi, gpio_cs, config)
         };
-        xpt2046::task(spi_device, &mutex_pos, embassy_time::Delay)
+        xpt2046::task(spi_device, &shared_pos, embassy_time::Delay)
     };
     let mut display = {
         use mipidsi::options::{Orientation, Rotation, ColorOrder};
@@ -98,162 +129,14 @@ async fn main(_spawner: embassy_executor::Spawner) {
         .build();
     let fut_main = async {
         loop {
-            mutex_pos.lock(|pos| {
-                if let Some(pos) = *pos.borrow() {
-                    eg::primitives::Rectangle::new(
-                        Point::new(pos.x - 4, pos.y - 4),
-                        Size::new(8, 8)
-                    ).into_styled(style_dot).draw(&mut display).unwrap();
-                }
-            });
+            if let Some(pos) = shared_pos.get_pos() {
+                eg::primitives::Rectangle::new(
+                    Point::new(pos.x - 4, pos.y - 4),
+                    Size::new(8, 8)
+                ).into_styled(style_dot).draw(&mut display).unwrap();
+            }
             time::Timer::after_millis(30).await;
         }
     };
     futures::join::join(fut_main, fut_touch).await;
-}
-
-mod xpt2046 {
-    use embedded_hal_1 as hal;
-    use embedded_hal_async as hal_async;
-    use core::cell::RefCell;
-    #[derive(Debug, Clone, Copy)]
-    pub struct Pos {
-        pub x: i32,
-        pub y: i32,
-    }
-
-    pub type MutexPos = embassy_sync::blocking_mutex::Mutex<
-        embassy_sync::blocking_mutex::raw::NoopRawMutex,
-        RefCell<Option<Pos>>>;
-
-    pub async fn task(spi_device: impl hal::spi::SpiDevice, mutex_pos: &MutexPos, mut delay: impl hal_async::delay::DelayNs) {
-        let mut touch = Driver::new(spi_device);
-        let mut idx_write = 0;
-        let mut idx_read = 0;
-        let mut pos_buf: [Pos; 8] = [Pos { x: 0, y: 0 }; 8];
-        loop {
-            if let Some((x, y)) = touch.read_pos() {
-                pos_buf[idx_write] = Pos { x, y };
-                idx_write = (idx_write + 1) % pos_buf.len();
-                if idx_read == idx_write {
-                    idx_read = (idx_read + 1) % pos_buf.len();
-                }
-                let mut xsum = 0;
-                let mut ysum = 0;
-                let mut count = 0;
-                let mut idx = idx_read;
-                while idx != idx_write {
-                    xsum += pos_buf[idx].x;
-                    ysum += pos_buf[idx].y;
-                    count += 1;
-                    idx = (idx + 1) % pos_buf.len();
-                }
-                mutex_pos.lock(|pos| {
-                    *pos.borrow_mut() = Some(Pos { x: xsum / count, y: ysum / count });
-                });
-            } else {
-                idx_write = 0;
-                idx_read = 0;
-                mutex_pos.lock(|pos| {
-                    *pos.borrow_mut() = None;
-                });
-            }
-            delay.delay_ms(10).await;
-            //embassy_time::Timer::after_millis(10).await;
-        }
-    }
-    struct SortedArray<const N: usize> {
-        data: [i32; N],
-        len: usize,
-    }
-    impl<const N: usize> SortedArray<N> {
-        fn new() -> Self {
-            Self { data: [0; N], len: 0 }
-        }
-        fn push(&mut self, elem: i32){
-            let mut idx = 0;
-            while idx < self.len && self.data[idx] < elem {
-                idx += 1;
-            }
-            if idx < N {
-                for j in (idx + 1..self.len).rev() {
-                    self.data[j] = self.data[j - 1];
-                }
-                self.data[idx] = elem;
-                if self.len < N {
-                    self.len += 1;
-                }
-            }
-        }
-        fn median(&self) -> Option<i32> {
-            let idx_mid = self.len / 2;
-            if self.len >= 3 {
-                Some((self.data[idx_mid] + self.data[idx_mid - 1] + self.data[idx_mid + 1]) / 3)
-            } else if self.len == 2 {
-                Some((self.data[0] + self.data[1]) / 2)
-            } else if self.len == 1 {
-                Some(self.data[0])
-            } else {
-                None
-            }
-        }
-    }
-    struct Calibration {
-        xraw_max: i32,
-        xraw_min: i32,
-        yraw_min: i32,
-        yraw_max: i32,
-        x_range: i32,
-        y_range: i32,
-    }
-    impl Calibration {
-        fn calc_pos(&self, xraw: i32, yraw: i32) -> (i32, i32) {
-            let x = ((xraw - self.xraw_min) * self.x_range /
-                (self.xraw_max - self.xraw_min)).clamp(0, self.x_range);
-            let y = ((yraw - self.yraw_min) * self.y_range /
-                (self.yraw_max - self.yraw_min)).clamp(0, self.y_range);
-            (x, y)
-        }
-    }
-    const CALIBRATION: Calibration = Calibration {
-        xraw_min: 0x00c8,
-        xraw_max: 0x0760,
-        yraw_min: 0x00d0,
-        yraw_max: 0x06d0,
-        x_range: 320,
-        y_range: 240,
-    };
-    pub struct Driver<SpiDevice> {
-        spi_device: SpiDevice,
-    }
-    impl<SpiDevice: hal::spi::SpiDevice> Driver<SpiDevice> {
-        pub fn new(spi_device: SpiDevice) -> Self {
-            Self { spi_device }
-        }
-        pub fn read_pos(&mut self) -> Option<(i32, i32)> {
-            let mut xbytes = [0u8; 2];
-            let mut ybytes = [0u8; 2];
-            let mut zbytes = [0u8; 1];
-            self.spi_device.transaction(&mut [
-                hal::spi::Operation::Write(&[Self::compose_cmd(0b001, 0b0, 0b1, 0b01)]), // x
-                hal::spi::Operation::Read(&mut xbytes),
-                hal::spi::Operation::Write(&[Self::compose_cmd(0b101, 0b0, 0b1, 0b01)]), // y
-                hal::spi::Operation::Read(&mut ybytes),
-                hal::spi::Operation::Write(&[Self::compose_cmd(0b011, 0b1, 0b1, 0b01)]), // z
-                hal::spi::Operation::Read(&mut zbytes),
-            ]).unwrap();
-            //defmt::info!("xbytes: {:02x}, ybytes: {:02x}, zbytes: {:02x}", xbytes, ybytes, zbytes);
-            let xraw = (((xbytes[0] as i32) << 4) | (xbytes[1] as i32 >> 4)) as i32;
-            let yraw = (((ybytes[0] as i32) << 4) | (ybytes[1] as i32 >> 4)) as i32;
-            if zbytes[0] < 3 {
-                None
-            } else {
-                defmt::info!("xraw: {:04x}, yraw: {:04x}, zbytes: {:02x}", xraw, yraw, zbytes);
-                Some(CALIBRATION.calc_pos(xraw, yraw))
-            }
-        }
-        const fn compose_cmd(adc: u8, mode: u8, reference: u8, power_down_mode: u8) -> u8 {
-            (0b1 << 7) | (adc << 4) | (mode << 3) | (reference << 2) | (power_down_mode << 0)
-        }
-    }
 }
