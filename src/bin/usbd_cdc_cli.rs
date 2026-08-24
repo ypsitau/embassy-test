e6b80eb267674cb4214cc991c5faf1dd4aa65912
ypsitau <ypsitau@nifty.com>
2026-08-24 15:19:56 +0900

    implementing cdc_cli

--- src/bin/usbd_cdc_cli.rs
+++ src/bin/usbd_cdc_cli.rs
@@ -3,11 +3,25 @@
 
 use core::sync::atomic;
 use defmt::info;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::{Channel, Receiver, Sender};
 use embassy_executor::Spawner;
 use embassy_rp as rp;
 use embassy_usb as usb;
+use embedded_cli::{cli::CliBuilder, Command};
 use static_cell::StaticCell;
+use ufmt::uwriteln;
 use {defmt_rtt as _, panic_probe as _};
+
+static OUTPUT: Channel<ThreadModeRawMutex, u8, 256> = Channel::new();
+
+#[derive(Command)]
+enum CommandLine<'a> {
+    /// Show a greeting.
+    Hello { name: Option<&'a str> },
+    /// Show the firmware status.
+    Status,
+}
 
 rp::bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => rp::usb::InterruptHandler<rp::peripherals::USB>;
@@ -40,7 +54,7 @@ async fn main(_spawner: Spawner) {
         usb_builder.handler(DEVICE_HANDLER.init(DeviceHandler::new()));
         usb_builder
     };
-    let (mut cdc_acm_sender, mut cdc_acm_receiver) = {
+    let (cdc_acm_sender, mut cdc_acm_receiver) = {
         static STATE: StaticCell<usb::class::cdc_acm::State> = StaticCell::new();
         let state = STATE.init(usb::class::cdc_acm::State::new());
         let max_packet_size = 64;
@@ -48,22 +62,103 @@ async fn main(_spawner: Spawner) {
     };
     let mut usb_device = usb_builder.build();
     let fut_usb = usb_device.run();
-    let fut_echo= async {
-        let mut buf = [0u8; 64];
+    let fut_output = output_task(cdc_acm_sender, OUTPUT.receiver());
+    let fut_cli = async {
+        let writer = WriterCDCACM { output: OUTPUT.sender() };
+        let mut cli = match CliBuilder::default()
+            .writer(writer)
+            .command_buffer([0; 128])
+            .history_buffer([0; 32])
+            .build()
+        {
+            Ok(cli) => cli,
+            Err(_) => return,
+        };
+        let mut packet_buf = [0u8; 64];
         let e = loop {
             cdc_acm_receiver.wait_connection().await;
             info!("Connected");
             let e = loop {
-                let buf_read = match cdc_acm_receiver.read_packet(&mut buf).await {
-                    Ok(n) => &buf[..n], Err(e) => break e,
+                let buf_read = match cdc_acm_receiver.read_packet(&mut packet_buf).await {
+                    Ok(n) => &packet_buf[..n], Err(e) => break e,
                 };
-                if let Err(e) = cdc_acm_sender.write_packet(buf_read).await { break e; }
+                for &byte in buf_read {
+                    let _ = cli.process_byte::<CommandLine, _>(
+                        byte,
+                        &mut CommandLine::processor(|cli, command| {
+                            match command {
+                                CommandLine::Hello { name } => {
+                                    uwriteln!(cli.writer(), "Hello, {}!", name.unwrap_or("world"))?;
+                                }
+                                CommandLine::Status => {
+                                    uwriteln!(cli.writer(), "status: ok")?;
+                                }
+                            }
+                            Ok(())
+                        }),
+                    );
+                }
             };
             if e != usb::driver::EndpointError::Disabled { break e; }
         };
         panic!("USB error: {:?}", e);
     };
-    embassy_futures::join::join(fut_usb, fut_echo).await;
+    embassy_futures::join::join(fut_usb, embassy_futures::join::join(fut_output, fut_cli)).await;
+}
+
+async fn output_task(
+    mut sender: usb::class::cdc_acm::Sender<'static, rp::usb::Driver<'static, rp::peripherals::USB>>,
+    receiver: Receiver<'static, ThreadModeRawMutex, u8, 256>,
+) -> ! {
+    let mut packet = [0u8; 64];
+    loop {
+        let first = receiver.receive().await;
+        let mut length = 0;
+        packet[length] = first;
+        length += 1;
+        while length < packet.len() {
+            match receiver.try_receive() {
+                Ok(byte) => {
+                    packet[length] = byte;
+                    length += 1;
+                }
+                Err(_) => break,
+            }
+        }
+        let _ = sender.write_packet(&packet[..length]).await;
+    }
+}
+
+//-----------------------------------------------------------------------------s
+// WriterCDCACM
+//-----------------------------------------------------------------------------s
+struct WriterCDCACM<'d> {
+    output: Sender<'d, ThreadModeRawMutex, u8, 256>,
+}
+
+impl embedded_io::ErrorType for WriterCDCACM<'_> {
+    type Error = WriterError;
+}
+
+impl embedded_io::Write for WriterCDCACM<'_> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+        for &byte in buf {
+            self.output.try_send(byte).map_err(|_| WriterError)?;
+        }
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct WriterError;
+
+impl embedded_io::Error for WriterError {
+    fn kind(&self) -> embedded_io::ErrorKind {
+        embedded_io::ErrorKind::Other
+    }
 }
 
 //-----------------------------------------------------------------------------
