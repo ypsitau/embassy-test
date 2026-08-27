31babc383eceaa9de7b57575bf5695fdcc230c61
ypsitau <ypsitau@nifty.com>
2026-08-27 16:31:48 +0900

    implementing

--- src/bin/pio_ws2812.rs
+++ src/bin/pio_ws2812.rs
@@ -6,64 +6,52 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::bind_interrupts;
-use embassy_rp::peripherals::{DMA_CH0, PIO0};
-use embassy_rp::pio::{InterruptHandler, Pio};
-use embassy_rp::pio_programs::ws2812::{PioWs2812, PioWs2812Program};
+use embassy_rp as rp;
 use embassy_time::{Duration, Ticker};
 use smart_leds::RGB8;
 use {defmt_rtt as _, panic_probe as _};
 
-bind_interrupts!(struct Irqs {
-    PIO0_IRQ_0 => InterruptHandler<PIO0>;
-    DMA_IRQ_0 => embassy_rp::dma::InterruptHandler<DMA_CH0>;
+rp::bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => rp::pio::InterruptHandler<rp::peripherals::PIO0>;
+    DMA_IRQ_0 => rp::dma::InterruptHandler<rp::peripherals::DMA_CH0>;
 });
 
 /// Input a value 0 to 255 to get a color value
 /// The colours are a transition r - g - b - back to r.
-fn wheel(mut wheel_pos: u8) -> RGB8 {
-    wheel_pos = 255 - wheel_pos;
-    if wheel_pos < 85 {
-        return (255 - wheel_pos * 3, 0, wheel_pos * 3).into();
+fn wheel(pos: usize) -> RGB8 {
+    let pos: u8 = pos as u8;
+    if pos < 85 {
+        RGB8::new(255 - pos * 3, 0, pos * 3)
+    } else if pos < 170 {
+        let pos = pos - 85;
+        RGB8::new(0, pos * 3, 255 - pos * 3)
+    } else {
+        let pos = pos - 170;
+        RGB8::new(pos * 3, 255 - pos * 3, 0)
     }
-    if wheel_pos < 170 {
-        wheel_pos -= 85;
-        return (0, wheel_pos * 3, 255 - wheel_pos * 3).into();
-    }
-    wheel_pos -= 170;
-    (wheel_pos * 3, 255 - wheel_pos * 3, 0).into()
 }
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    const NUM_LEDS: usize = 8;
     info!("Start");
     let p = embassy_rp::init(Default::default());
-
-    let Pio { mut common, sm0, .. } = Pio::new(p.PIO0, Irqs);
-
-    // This is the number of leds in the string. Helpfully, the sparkfun thing plus and adafruit
-    // feather boards for the 2040 both have one built in.
-    const NUM_LEDS: usize = 1;
-    let mut data = [RGB8::default(); NUM_LEDS];
-
-    // Common neopixel pins:
-    // Thing plus: 8
-    // Adafruit Feather: 16;  Adafruit Feather+RFM95: 4
-    let program = PioWs2812Program::new(&mut common);
-    let mut ws2812 = PioWs2812::new(&mut common, sm0, p.DMA_CH0, Irqs, p.PIN_16, &program);
-
-    // Loop forever making RGB values and pushing them out to the WS2812.
+    let mut pio0 = rp::pio::Pio::new(p.PIO0, Irqs);
+    let mut rgb8_array = [RGB8::default(); NUM_LEDS];
+    let mut ws2812 = {
+        let program = rp::pio_programs::ws2812::PioWs2812Program::new(&mut pio0.common);
+        rp::pio_programs::ws2812::PioWs2812::new(&mut pio0.common, pio0.sm0, p.DMA_CH0, Irqs, p.PIN_16, &program)
+    };
     let mut ticker = Ticker::every(Duration::from_millis(10));
     loop {
         for j in 0..(256 * 5) {
             debug!("New Colors:");
             for i in 0..NUM_LEDS {
-                data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
-                debug!("R: {} G: {} B: {}", data[i].r, data[i].g, data[i].b);
+                rgb8_array[i] = wheel(((i * 256) / NUM_LEDS + j) & 255);
+                debug!("R: {} G: {} B: {}", rgb8_array[i].r, rgb8_array[i].g, rgb8_array[i].b);
             }
-            ws2812.write(&data).await;
+            ws2812.write(&rgb8_array).await;
             //ws2812.write_slice(&data).await;
-
             ticker.next().await;
         }
     }
