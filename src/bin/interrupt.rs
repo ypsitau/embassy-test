4a426e144cb55bb0d6ff226fd57dabfeb096501c
ypsitau <ypsitau@nifty.com>
2026-08-29 14:58:42 +0900

    implementing

--- src/bin/interrupt.rs
+++ src/bin/interrupt.rs
@@ -22,56 +22,52 @@ use portable_atomic::{AtomicU32, Ordering};
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
-static COUNTER: AtomicU32 = AtomicU32::new(0);
-static PWM: Mutex<CriticalSectionRawMutex, RefCell<Option<rp::pwm::Pwm>>> = Mutex::new(RefCell::new(None));
-static ADC: Mutex<CriticalSectionRawMutex, RefCell<Option<(rp::adc::Adc<rp::adc::Blocking>, rp::adc::Channel)>>> =
-    Mutex::new(RefCell::new(None));
-static ADC_VALUES: Channel<CriticalSectionRawMutex, u16, 2048> = Channel::new();
+static ATOMIC_COUNTER: AtomicU32 = AtomicU32::new(0);
+static MUTEX_PWM: Mutex<CriticalSectionRawMutex, RefCell<Option<rp::pwm::Pwm>>> = Mutex::new(RefCell::new(None));
+static MUTEX_ADC: Mutex<CriticalSectionRawMutex, RefCell<Option<(rp::adc::Adc<rp::adc::Blocking>,
+    rp::adc::Channel)>>> = Mutex::new(RefCell::new(None));
+static CHANNEL_ADC: Channel<CriticalSectionRawMutex, u16, 2048> = Channel::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-
     let adc = rp::adc::Adc::new_blocking(p.ADC, Default::default());
-    let p26 = rp::adc::Channel::new_pin(p.PIN_26, rp::gpio::Pull::None);
-    ADC.lock(|a| a.borrow_mut().replace((adc, p26)));
-
+    let adc_ch = rp::adc::Channel::new_pin(p.PIN_26, rp::gpio::Pull::None);
+    MUTEX_ADC.lock(|a| a.borrow_mut().replace((adc, adc_ch)));
     let pwm = rp::pwm::Pwm::new_output_b(p.PWM_SLICE4, p.PIN_25, Default::default());
-    PWM.lock(|p| p.borrow_mut().replace(pwm));
-
+    MUTEX_PWM.lock(|p| p.borrow_mut().replace(pwm));
     // Enable the interrupt for pwm slice 4
     rp::pac::PWM.inte().modify(|w| w.set_ch4(true));
     unsafe {
         cortex_m::peripheral::NVIC::unmask(rp::interrupt::PWM_IRQ_WRAP);
     }
-
     // Tasks require their resources to have 'static lifetime
     // No Mutex needed when sharing within the same executor/prio level
-    static AVG: StaticCell<Cell<u32>> = StaticCell::new();
-    let avg = AVG.init(Default::default());
-    spawner.spawn(processing(avg).unwrap());
-
+    let avg = {
+        static STATIC_CELL: StaticCell<Cell<u32>> = StaticCell::new();
+        STATIC_CELL.init(Default::default())
+    };
+    spawner.spawn(task_processing(avg).unwrap());
     let mut ticker = Ticker::every(Duration::from_secs(1));
     loop {
         ticker.next().await;
-        let freq = COUNTER.swap(0, Ordering::Relaxed);
+        let freq = ATOMIC_COUNTER.swap(0, Ordering::Relaxed);
         info!("pwm freq: {:?} Hz", freq);
         info!("adc average: {:?}", avg.get());
-
         // Update the pwm duty cycle, based on the averaged adc reading
         let mut config = rp::pwm::Config::default();
         config.compare_b = ((avg.get() as f32 / 4095.0) * config.top as f32) as _;
-        PWM.lock(|p| p.borrow_mut().as_mut().unwrap().set_config(&config));
+        MUTEX_PWM.lock(|p| p.borrow_mut().as_mut().unwrap().set_config(&config));
     }
 }
 
 #[embassy_executor::task]
-async fn processing(avg: &'static Cell<u32>) {
+async fn task_processing(avg: &'static Cell<u32>) {
     let mut buffer: heapless::HistoryBuf<u16, 100> = Default::default();
     loop {
-        let val = ADC_VALUES.receive().await;
-        buffer.write(val);
-        let sum: u32 = buffer.iter().map(|x| *x as u32).sum();
+        let value = CHANNEL_ADC.receive().await;
+        buffer.write(value);
+        let sum: u32 = buffer.iter().map(|value| *value as u32).sum();
         avg.set(sum / buffer.len() as u32);
     }
 }
@@ -79,13 +75,12 @@ async fn processing(avg: &'static Cell<u32>) {
 #[interrupt]
 fn PWM_IRQ_WRAP() {
     critical_section::with(|cs| {
-        let mut adc = ADC.borrow(cs).borrow_mut();
-        let (adc, p26) = adc.as_mut().unwrap();
-        let val = adc.blocking_read(p26).unwrap();
-        ADC_VALUES.try_send(val).ok();
-
+        let mut adc_pack = MUTEX_ADC.borrow(cs).borrow_mut();
+        let (adc, adc_ch) = adc_pack.as_mut().unwrap();
+        let value = adc.blocking_read(adc_ch).unwrap();
+        CHANNEL_ADC.try_send(value).ok();
         // Clear the interrupt, so we don't immediately re-enter this irq handler
-        PWM.borrow(cs).borrow_mut().as_mut().unwrap().clear_wrapped();
+        MUTEX_PWM.borrow(cs).borrow_mut().as_mut().unwrap().clear_wrapped();
     });
-    COUNTER.fetch_add(1, Ordering::Relaxed);
+    ATOMIC_COUNTER.fetch_add(1, Ordering::Relaxed);
 }
