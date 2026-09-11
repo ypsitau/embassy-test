43634a3542d08e80c2d8d687d80f4931ff69c8c3
ypsitau <ypsitau@nifty.com>
2026-09-11 16:53:47 +0900

    implementing

--- src/bin/i2c_pca9685.rs
+++ src/bin/i2c_pca9685.rs
@@ -3,13 +3,8 @@
 
 use embassy_executor::Spawner;
 use embassy_rp as rp;
-use embassy_time::Timer;
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
-
-type MutexI2C1 = embassy_sync::mutex::Mutex<
-    embassy_sync::blocking_mutex::raw::NoopRawMutex,
-    rp::i2c::I2c<'static, rp::peripherals::I2C1, rp::i2c::Async>>;
 
 rp::bind_interrupts!(struct Irqs {
     I2C1_IRQ => rp::i2c::InterruptHandler<rp::peripherals::I2C1>;
@@ -17,18 +12,22 @@ rp::bind_interrupts!(struct Irqs {
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = rp::init(Default::default());
-    let mutex_i2c1 = {
-        let scl = p.PIN_15;
-        let sda = p.PIN_14;
-        let config = rp::i2c::Config::default();
-        // should be replaced by make_static macro when it becomes available
-        static STATIC_CELL: StaticCell<MutexI2C1> = StaticCell::new();
-        STATIC_CELL.init(MutexI2C1::new(rp::i2c::I2c::new_async(p.I2C1, scl, sda, Irqs, config)))
+    let (i2c1_device,) = {
+        type MutexI2C1 = embassy_sync::mutex::Mutex<
+            embassy_sync::blocking_mutex::raw::NoopRawMutex,
+            rp::i2c::I2c<'static, rp::peripherals::I2C1, rp::i2c::Async>>;
+        let p = rp::init(Default::default());
+        let mutex_i2c1 = {
+            let scl = p.PIN_15;
+            let sda = p.PIN_14;
+            let config = rp::i2c::Config::default();
+            static STATIC_CELL: StaticCell<MutexI2C1> = StaticCell::new();
+            STATIC_CELL.init(MutexI2C1::new(rp::i2c::I2c::new_async(p.I2C1, scl, sda, Irqs, config)))
+        };
+        let i2c1_device = embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice::new(mutex_i2c1);
+        (i2c1_device,)
     };
-    // impl embedded_hal_async::i2c::I2c
-    let i2c_device = embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice::new(mutex_i2c1);
-    let fut_pwm_task = task_pwm(i2c_device);
+    let fut_pwm_task = task_pwm(i2c1_device);
     fut_pwm_task.await;
 }
 
